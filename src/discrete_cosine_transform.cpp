b3f2fed89cfda4becbb5bf7515d0fec599df40fd
James Lee <jamesl33info@gmail.com>
2019-02-10 17:05:17 +0000

    Implemented EncodeChunkLength/DecodeChunkLength functions

--- src/discrete_cosine_transform.cpp
+++ src/discrete_cosine_transform.cpp
@@ -17,6 +17,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include "discrete_cosine_transform.hpp"
 
+const std::vector<std::vector<int>> luminance = {{16, 11, 10, 16, 24, 40, 51, 61},
+                                                 {12, 12, 14, 19, 26, 58, 60, 55},
+                                                 {14, 13, 16, 24, 40, 57, 69, 56},
+                                                 {14, 17, 22, 29, 51, 87, 80, 62},
+                                                 {18, 22, 37, 56, 68, 109, 103, 77},
+                                                 {24, 35, 55, 64, 81, 104, 113, 92},
+                                                 {49, 64, 78, 87, 103, 121, 120, 101},
+                                                 {72, 92, 95, 98, 112, 100, 103, 99}};
+
 /**
  * Encode the payload into the carrier image using DCT coefficient swapping.
  *
@@ -50,7 +59,123 @@ void DiscreteCosineTransform::EncodeChunk(const int& start, const std::vector<un
  * @param chunk_length The length of the next chunk in bytes.
  */
 void DiscreteCosineTransform::EncodeChunkLength(const int& start, const unsigned int& chunk_length) {
+    int bits_written = 0;
 
+    // Convert the image to the YCrCb color space.
+    cv::Mat image_ycrcb;
+    cv::cvtColor(this -> image, image_ycrcb, cv::COLOR_BGR2YCrCb);
+
+    // Convert the image to floating point.
+    cv::Mat image_ycrcb_fp;
+    image_ycrcb.convertTo(image_ycrcb_fp, CV_32F);
+
+    // Split the image into channels
+    std::vector<cv::Mat> channels;
+    cv::split(image_ycrcb_fp, channels);
+
+    for (int row = 0; row < image_ycrcb_fp.rows - 8; row += 8) {
+        for (int col = 0; col < image_ycrcb_fp.cols - 8; col += 8) {
+            if (row == 0 && col == 0) {
+                row = start / (image_ycrcb_fp.cols / 8) * 8;
+                col = start % (image_ycrcb_fp.cols / 8) * 8;
+            }
+
+            // We only need to encode a 32bit integer, stop once complete.
+            if (bits_written == 32) {
+                // Merge the image channels.
+                cv::Mat merged_ycrcb_fp;
+                cv::merge(channels, merged_ycrcb_fp);
+
+                // Convert the image to unsigned char.
+                cv::Mat merged_ycrcb;
+                merged_ycrcb_fp.convertTo(merged_ycrcb, CV_8U);
+
+                // Convert the image to the BGR colorspace.
+                cv::cvtColor(merged_ycrcb, this -> image, cv::COLOR_YCrCb2BGR);
+                return;
+            }
+
+            // The current 8x8 block we are working on.
+            cv::Mat block(channels[0], cv::Rect(col, row, 8, 8));
+
+            // A "working" copy of the block.
+            cv::Mat trans(cv::Size(8, 8), block.type());
+
+            // Centre the coefficients around 0.
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    block.at<float>(x, y) -= 128;
+                }
+            }
+
+            // Perform forward dct.
+            cv::dct(block, trans);
+
+            // Quantize the 8x8 block while rounding the values.
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    trans.at<float>(x, y) = float(int(trans.at<float>(x, y) / (luminance[x][y] / 10)));
+                }
+            }
+
+            int bit = this -> GetBit(chunk_length, bits_written);
+
+            // TODO(James Lee) - Swap multiple coefficients.
+            float low = trans.at<float>(2, 0);
+            float high = trans.at<float>(0, 2);
+
+            // Swap the dct coefficients to match the data bit.
+            if (bit) {
+                if (low == high) {
+                    low -= 0.5;
+                    high += 0.5;
+                } else if (low > high) {
+                    std::swap(low, high);
+                }
+            } else {
+                if (low == high) {
+                    low += 0.5;
+                    high -= 0.5;
+                } else if (low < high) {
+                    std::swap(low, high);
+                }
+            }
+
+            // TODO(James Lee) - Expose this option to the user.
+            // Increase the difference between the "important" coefficients.
+            // A larger value will distort the image more, however, it will
+            // make the data more robust.
+            if (low < high) {
+                low -= 5;
+                high += 5;
+            } else {
+                low += 5;
+                high -= 5;
+            }
+
+            trans.at<float>(2, 0) = low;
+            trans.at<float>(0, 2) = high;
+
+            // Dequantize the 8x8 block.
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    trans.at<float>(x, y) *= (luminance[x][y] / 10);
+                }
+            }
+
+            // Performce the inverse dct.
+            cv::idct(trans, block);
+
+            // Center the coefficients around 128.
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    block.at<float>(x, y) += 128;
+                }
+            }
+
+            bits_written++;
+        }
+    }
 }
 
 /**
@@ -71,5 +196,56 @@ std::vector<unsigned char> DiscreteCosineTransform::DecodeChunk(const int& start
  * @return The length of the next chunk in bytes.
  */
 unsigned int DiscreteCosineTransform::DecodeChunkLength(const int& start) {
+    int bits_read = 0;
+    unsigned int chunk_length = 0;
+
+    // Convert the image to the YCrCb color space.
+    cv::Mat image_ycrcb;
+    cv::cvtColor(this -> image, image_ycrcb, cv::COLOR_BGR2YCrCb);
+
+    // Convert the image to floating point.
+    cv::Mat image_ycrcb_fp;
+    image_ycrcb.convertTo(image_ycrcb_fp, CV_32F);
+
+    // Split the image into channels.
+    std::vector<cv::Mat> channels;
+    cv::split(image_ycrcb_fp, channels);
+
+    for (int row = 0; row < image_ycrcb_fp.rows - 8; row += 8) {
+        for (int col = 0; col < image_ycrcb_fp.cols - 8; col += 8) {
+            if (row == 0 && col == 0) {
+                row = start / (image_ycrcb_fp.cols / 8) * 8;
+                col = start % (image_ycrcb_fp.cols / 8) * 8;
+            }
+
+            if (bits_read == 32) {
+                // We only need to decode a 32bit integer, stop once complete.
+                return chunk_length;
+            }
+
+            // The current 8x8 block we are working on.
+            cv::Mat block(channels[0], cv::Rect(col, row, 8, 8));
+
+            // A "working" copy of the block.
+            cv::Mat trans(cv::Size(8, 8), block.type());
+
+            // Perform the forward dct.
+            cv::dct(block, trans);
+
+            // Round the values in the 8x8 block.
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    trans.at<float>(x, y) = float(int(trans.at<float>(x, y)));
+                }
+            }
+
+            // TODO(James Lee) - Read from multiple swapped coefficients.
+            // Decode the data from the swapped coefficients.
+            this -> SetBit(&chunk_length, bits_read, (trans.at<float>(0, 2) > trans.at<float>(2, 0)));
+
+            bits_read++;
+        }
+    }
+
     return 0; // This "should" not be reached
 }
