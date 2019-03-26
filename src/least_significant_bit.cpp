daba02547ce514fb33395f17449a21eb5ce1ad24
James Lee <jamesl33info@gmail.com>
2019-03-26 21:04:52 +0000

    Dynamically determine whether or not to use multithread encoding

--- src/least_significant_bit.cpp
+++ src/least_significant_bit.cpp
@@ -31,52 +31,42 @@ void LeastSignificantBit::Encode(const boost::filesystem::path &payload_path)
     std::string filename = payload_path.filename().string();
     std::vector<unsigned char> filename_bytes(filename.begin(), filename.end());
 
-    std::vector<std::thread> threads;
-
     // Encode the filename length into the carrier image
-    threads.emplace_back(
-            &LeastSignificantBit::EncodeChunkLength,
-            this,
-            0,
-            filename_bytes.size());
+    this->EncodeChunkLength(0, filename_bytes.size());
 
     // Encode the filename into the carrier image
-    for (int i = 0; i < (NUM_THREADS / 2); i++)
-    {
-        threads.emplace_back(
-                &LeastSignificantBit::EncodeChunk,
-                this,
-                32 + (((filename_bytes.size() / (NUM_THREADS / 2)) * 8) * i),
-                filename_bytes.begin() + ((filename_bytes.size() / (NUM_THREADS / 2)) * i),
-                filename_bytes.end() - ((filename_bytes.size() / (NUM_THREADS / 2)) * (((NUM_THREADS / 2) - 1) - i)));
-    }
+    this->EncodeChunk(32, filename_bytes.begin(), filename_bytes.end());
 
     // Read the payload into a vector<unsigned char>
     std::vector<unsigned char> payload_bytes = this->ReadPayload(payload_path);
 
     // Encode the payload length into the carrier image
-    threads.emplace_back(
-            &LeastSignificantBit::EncodeChunkLength,
-            this,
-            32 + filename_bytes.size() * 8,
-            payload_bytes.size());
+    this->EncodeChunkLength(32 + (filename_bytes.size() * 8), payload_bytes.size());
 
-    // Encode the payload into the carrier image
-    for (int i = 0; i < NUM_THREADS; i++)
+    // Encode the payload into the carrier image, multithread if payload is larger than 10kb
+    if (payload_bytes.size() < 10240)
     {
-        threads.emplace_back(
-                &LeastSignificantBit::EncodeChunk,
-                this,
-                64 + (filename_bytes.size() * 8) + (((payload_bytes.size() / NUM_THREADS) * 8) * i),
-                payload_bytes.begin() + ((payload_bytes.size() / NUM_THREADS) * i),
-                payload_bytes.end() - ((payload_bytes.size() / NUM_THREADS) * ((NUM_THREADS - 1) - i)));
+        this->EncodeChunk(64 + (filename_bytes.size() * 8), payload_bytes.begin(), payload_bytes.end());
     }
-
-
-    // Wait for all the threads to finish encoding
-    for (std::thread &thr : threads)
+    else
     {
-        thr.join();
+        std::vector<std::thread> threads;
+
+        for (int i = 0; i < NUM_THREADS; i++)
+        {
+            threads.emplace_back(
+                    &LeastSignificantBit::EncodeChunk,
+                    this,
+                    64 + (filename_bytes.size() * 8) + (((payload_bytes.size() / NUM_THREADS) * 8) * i),
+                    payload_bytes.begin() + ((payload_bytes.size() / NUM_THREADS) * i),
+                    payload_bytes.end() - ((payload_bytes.size() / NUM_THREADS) * ((NUM_THREADS - 1) - i)));
+        }
+
+        // Wait for all the threads to finish encoding
+        for (std::thread &thr : threads)
+        {
+            thr.join();
+        }
     }
 
     // Write the steganographic image
