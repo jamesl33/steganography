79b900f4b2bbb5b80c0f9970cbb80ca6a9b6ad38
James Lee <jamesl33info@gmail.com>
2019-04-04 12:37:33 +0100

    Updated documentation

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['encode',['Encode',['../classDiscreteCosineTransform.html#a70576aafcd9737695f788c442bb32601',1,'DiscreteCosineTransform::Encode()'],['../classLeastSignificantBit.html#a4b785810c29190d2661029ea6502dcad',1,'LeastSignificantBit::Encode()'],['../classSteganography.html#a939a59c57545a87848db73172ae35981',1,'Steganography::Encode()']]],
   ['encodechunk',['EncodeChunk',['../classDiscreteCosineTransform.html#ab6160f1152f047d42a81c9b583efa29c',1,'DiscreteCosineTransform::EncodeChunk()'],['../classLeastSignificantBit.html#aea622da7d7f53ee564a26cbfb5c090a3',1,'LeastSignificantBit::EncodeChunk()']]],
-  ['encodechunklength',['EncodeChunkLength',['../classDiscreteCosineTransform.html#a4379cf82f13bac3e0d337214566204bc',1,'DiscreteCosineTransform::EncodeChunkLength()'],['../classLeastSignificantBit.html#ab0ec51ac95eb01e835e45b541fe780fd',1,'LeastSignificantBit::EncodeChunkLength()']]],
+  ['encodechunklength',['EncodeChunkLength',['../classDiscreteCosineTransform.html#a4379cf82f13bac3e0d337214566204bc',1,'DiscreteCosineTransform::EncodeChunkLength()'],['../classLeastSignificantBit.html#a09380f245ec5d67e89443e49c5f97d37',1,'LeastSignificantBit::EncodeChunkLength()']]],
   ['encodeexception',['EncodeException',['../classEncodeException.html',1,'EncodeException'],['../classEncodeException.html#a61386849dd82c677c527f7370c3a967a',1,'EncodeException::EncodeException()']]]
 ];
